e501f4f0c0739daecbb2b533eab7693899affaeb
JhonSnow <jhon.vallecilla@alumnos.ucn.cl>
2025-10-31 14:35:10 -0300

    chore: add initial setup in Program.cs

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -1,7 +1,19 @@
+using ApiGateway.Protos.Inventory;
+using DotNetEnv;
+
+Env.Load();
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 builder.Services.AddControllers();
+
+builder.Services.AddGrpcClient<InventoryService.InventoryServiceClient>(options =>
+{
+    options.Address = new Uri(Environment.GetEnvironmentVariable("INVENTORY_SERVICE_URL") ?? "https://localhost:5001");
+});
 
 var app = builder.Build();
 
